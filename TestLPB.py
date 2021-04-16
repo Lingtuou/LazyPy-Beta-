13da422a65bab61601b1a6fbaaf961329613d36c
Lingtuou <74095599+Lingtuou@users.noreply.github.com>
2021-04-16 22:47:29 +0800

    Add files via upload

---
+++ TestLPB.py
@@ -0,0 +1,15 @@
+#TestLPB
+import LazyPy_Basic as Lb
+
+Lb.p('Hello LPB')
+Lb.ts(3)
+Lb.p('Hello again')
+
+l = Lb.ip('What your name?')
+
+Lb.p(l)
+
+Lb.p(Lb.gd())
+
+Lb.p(Lb.tt())
+Lb.p(Lb.cm(2021, 1))
